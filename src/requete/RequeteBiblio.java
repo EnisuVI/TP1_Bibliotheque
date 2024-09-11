622bc378a976d1913144a15cf78f9d474409b5b5
Charly <66905063+EnisuVI@users.noreply.github.com>
2024-09-11 16:57:57 +0200

    Questions 8 et 9

--- src/requete/RequeteBiblio.java
+++ src/requete/RequeteBiblio.java
@@ -57,11 +57,60 @@ public class RequeteBiblio {
         return utilisateurs;
     }
 
+    public List<Livre> livresEmpruntes() throws SQLException {
+        String requete = "SELECT * FROM livre INNER JOIN emprunt ON livre.isbn=emprunt.fk_isbn\n" +
+                "WHERE date_retour IS NULL ORDER BY titre";
+        List<Livre> livresEmpruntes = new ArrayList<>();
+        Statement statement = this.connection.createStatement();
+        ResultSet result = statement.executeQuery(requete);
+        while (result.next()) {
+            String titre = result.getString("titre");
+            long isbn = result.getLong("isbn");
+            String auteur = result.getString("auteur");
+            String editeur = result.getString("editeur");
+            int nbPages = result.getInt("nb_pages");
+            int date = result.getInt("annee_publication");
+            Livre lesLivres = new Livre(isbn, titre, auteur, editeur, nbPages, date);
+            livresEmpruntes.add(lesLivres);
+        }
+        result.close();
+        statement.close();
+        return livresEmpruntes;
+    }
+
+    public List<Livre> livreRecherche(String recherche) throws SQLException {
+        String requete = "SELECT * FROM livre WHERE titre LIKE ? OR auteur LIKE ? OR editeur LIKE ?";
+        List<Livre> livreRecherche = new ArrayList<>();
+        PreparedStatement prepStatement = this.connection.prepareStatement(requete);
+        prepStatement.setString(1, "%" + recherche + "%");
+        prepStatement.setString(2, "%" + recherche + "%");
+        prepStatement.setString(3, "%" + recherche + "%");
+        ResultSet result = prepStatement.executeQuery();
+        while (result.next()) {
+            String titre = result.getString("titre");
+            long isbn = result.getLong("isbn");
+            String auteur = result.getString("auteur");
+            String editeur = result.getString("editeur");
+            int nbPages = result.getInt("nb_pages");
+            int date = result.getInt("annee_publication");
+            Livre livre = new Livre(isbn, titre, auteur, editeur, nbPages, date);
+            livreRecherche.add(livre);
+            }
+        result.close();
+        prepStatement.close();
+        return livreRecherche;
+        }
+
     public static void main(String[] args) throws SQLException {
         RequeteBiblio connection1 = new RequeteBiblio();
         List<Livre> livres = connection1.biblioAlpha();
         System.out.println(livres);
         List<Usager> utilsateurs = connection1.getUtilisateurs();
         System.out.println(utilsateurs);
+        List<Livre> livresEmpruntes = connection1.livresEmpruntes();
+        System.out.println(livresEmpruntes);
+
+        List<Livre> livreRecherche = connection1.livreRecherche("Vieux");
+        System.out.println(livreRecherche);
     }
 }
