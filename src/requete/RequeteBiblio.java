e29ed7a797b2ab4eab317084f8f0681011465a27
Charly <66905063+EnisuVI@users.noreply.github.com>
2024-09-11 17:32:43 +0200

    Questions 10 et 11

--- src/requete/RequeteBiblio.java
+++ src/requete/RequeteBiblio.java
@@ -1,5 +1,6 @@
 package requete;
 
+import modele.Emprunt;
 import modele.Livre;
 import modele.Usager;
 
@@ -18,6 +19,7 @@ public class RequeteBiblio {
         this.connection = connexionBdd;
     }
 
+    //Question 6
     public List<Livre> biblioAlpha() throws SQLException {
         String requete = "SELECT * FROM livre ORDER BY titre ASC";
         List<Livre> livres = new ArrayList<>();
@@ -38,6 +40,7 @@ public class RequeteBiblio {
         return livres;
     }
 
+    //Question 7
         public List<Usager> getUtilisateurs() throws SQLException {
         String requete = "SELECT * FROM usager ORDER BY nom ASC";
         List<Usager> utilisateurs = new ArrayList<>();
@@ -57,6 +60,7 @@ public class RequeteBiblio {
         return utilisateurs;
     }
 
+    //Question 8
     public List<Livre> livresEmpruntes() throws SQLException {
         String requete = "SELECT * FROM livre INNER JOIN emprunt ON livre.isbn=emprunt.fk_isbn\n" +
                 "WHERE date_retour IS NULL ORDER BY titre";
@@ -78,6 +82,7 @@ public class RequeteBiblio {
         return livresEmpruntes;
     }
 
+    // Question 9
     public List<Livre> livreRecherche(String recherche) throws SQLException {
         String requete = "SELECT * FROM livre WHERE titre LIKE ? OR auteur LIKE ? OR editeur LIKE ?";
         List<Livre> livreRecherche = new ArrayList<>();
@@ -101,6 +106,47 @@ public class RequeteBiblio {
         return livreRecherche;
         }
 
+        //Question 10
+        public List<Usager> personneRecherche(String recherche) throws SQLException {
+        String requete = "SELECT * FROM usager WHERE nom LIKE ? OR prenom LIKE ?";
+        List<Usager> personneRecherche = new ArrayList<>();
+        PreparedStatement prepStatement = this.connection.prepareStatement(requete);
+        prepStatement.setString(1, "%" + recherche + "%");
+        prepStatement.setString(2, "%" + recherche + "%");
+        ResultSet result = prepStatement.executeQuery();
+        while (result.next()) {
+            int id_usager = result.getInt("id_usager");
+            String nom = result.getString("nom");
+            String prenom = result.getString("prenom");
+            String adresse = result.getString("adresse");
+            Date date_naissance = result.getDate("date_naissance");
+            Usager usager = new Usager(id_usager, nom, prenom, adresse, date_naissance);
+            personneRecherche.add(usager);
+            }
+        result.close();
+        prepStatement.close();
+        return personneRecherche;
+        }
+
+        //Question 11
+        public List<Emprunt> enRetard(int rechercheDate) throws SQLException {
+        String requete = "SELECT * FROM emprunt WHERE DATEDIFF(CURRENT_DATE, date_emprunt) >= ? AND date_retour IS NULL";
+        List<Emprunt> enRetard = new ArrayList<>();
+        PreparedStatement prepStatement = this.connection.prepareStatement(requete);
+        prepStatement.setInt(1, rechercheDate);
+        ResultSet result = prepStatement.executeQuery();
+        while (result.next()) {
+            int id_emprunt = result.getInt("id_emprunt");
+            Date date_emprunt = result.getDate("date_emprunt");
+            Date date_retour = result.getDate("date_retour");
+            Emprunt emprunt = new Emprunt(id_emprunt, date_emprunt, date_retour, null, null);
+            enRetard.add(emprunt);
+            }
+        result.close();
+        prepStatement.close();
+        return enRetard;
+        }
+
     public static void main(String[] args) throws SQLException {
         RequeteBiblio connection1 = new RequeteBiblio();
         List<Livre> livres = connection1.biblioAlpha();
@@ -112,5 +158,9 @@ public class RequeteBiblio {
 
         List<Livre> livreRecherche = connection1.livreRecherche("Vieux");
         System.out.println(livreRecherche);
+        List<Usager> personneRecherche = connection1.personneRecherche("Chantal");
+        System.out.println(personneRecherche);
+        List<Emprunt> enRetard = connection1.enRetard(20);
+        System.out.println(enRetard);
     }
 }
